4abbf0d8b43624a9c241f8ec82f793a93308776f
김예빈 <kyb0215@flow.team>
2026-03-13 16:07:12 +0900

    프론트 백 연결

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                // 프론트에서 /api로 시작하는 요청을 보내면
+                source: "/api/:path*",
+                // 백엔드(8080포트)로 몰래 연결해줘!
+                destination: "http://localhost:8080/api/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
