be7fff4ead11e9bee0bd874a013418e9ba8d674e
김예빈 <kyb0215@flow.team>
2026-03-13 12:02:51 +0900

    페이지네이션 및 검색

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,53 @@ const pool = new Pool({
     port: 5432
 });
 
-app.get('/test', async (req, res) => {
+app.get('/api/posts', async (req, res) => {
     try{
-        const result = await pool.query('SELECT NOW()');
+        // 페이지네이션 및 검색
+        const page = parseInt(req.query.page || 1);
+        const search = req.query.search || '';
+        const limit = 3;
+        const offset = (page - 1) * limit;
+
+        const postsQuery = `
+            SELECT * FROM board_posts
+            WHERE title ILIKE $1
+            ORDER BY created_at DESC
+            LIMIT $2 OFFSET $3
+        `;
+        const postsResult = await pool.query(postsQuery, [`%${search}`, limit, offset]);
+
+        // 전체 개수
+        const countQuery = `SELECT COUNT(*) FROM board_posts WHERE title ILIKE $1`;
+        const countResult = await pool.query(countQuery, [`%${search}%`]);
+        const totalCount = parseInt(countResult.rows[0].count);
+
         res.json({
-            message: "DB 연결 성공",
-            dbTime: result.rows[0].now
+            posts: postsResult.rows,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: page,
+            totalCount: totalCount
         });
+
     } catch (err){
         console.error(err);
-        res.status(500).json({message: "DB 연결 실패", error: err.message});
+        res.status(500).json({message: "데이터 조회 실패", error: err.message});
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8080, () => {
     console.log("백엔드 서버가 8080번 포트에서 실행 중")
